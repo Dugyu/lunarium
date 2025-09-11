2ac14e9a5fdb7fbc88bf2fa4edb1b00013866325
Dugyu <23237626+Dugyu@users.noreply.github.com>
2025-09-11 17:50:45 +0800

    fix: mockup transform, always center aligned

--- apps/studio/src/components/frame/frame.tsx
+++ apps/studio/src/components/frame/frame.tsx
@@ -8,7 +8,7 @@ interface FrameProps {
 function Frame({ children, className }: FrameProps) {
   return (
     <div
-      className={`relative w-full h-full min-h-[480px] min-w-[480px] bg-[#f5f5f5] flex flex-row justify-center items-center box-border ${
+      className={`w-full h-full min-h-[480px] min-w-[480px] bg-[#f5f5f5] flex flex-row justify-center items-center box-border ${
         className ?? ''
       }`}
     >
--- apps/studio/src/components/studio-frame/studio-frame.tsx
+++ apps/studio/src/components/studio-frame/studio-frame.tsx
@@ -4,10 +4,10 @@ import { Mockup } from '@/components/mockup';
 function StudioFrame() {
   return (
     <Frame>
-      <div className='flex-1'>
+      <div className='flex-1 flex justify-center items-center'>
         <Mockup className='bg-black' />
       </div>
-      <div className='flex-1'>
+      <div className='flex-1 flex justify-center items-center'>
         <Mockup className='bg-white' />
       </div>
     </Frame>
